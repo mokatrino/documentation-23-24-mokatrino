806536a5308fe67d742c2028350e2b014fa9b814
mokatrino <ka.monkova@hbk-bs.de>
2024-03-14 11:37:31 +0100

    add files
    add new files, update script, delete not needed files

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 	console.log("Hello World");
 });
+
+window.addEventListener('scroll', function () {
+	console.log(window.scrollY);
+	var header = document.querySelector('nav');
+	header.style.opacity = window.scrollY > 0 && (window.scrollY < 750 || window.scrollY > 900) && (window.scrollY < 1600 || window.scrollY > 1750) ? 0 : 1;
+});
